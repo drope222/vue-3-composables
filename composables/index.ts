58bd9a0d55a3e4a09bda1602c020c738735b0145
Pedro Melo <pedro.meloo@hotmail.com>
2022-06-03 16:50:53 -0300

    fix: onUseClickOutside export

--- composables/index.ts
+++ composables/index.ts
@@ -2,5 +2,6 @@ import { useDark } from './useDark'
 import { onKeyPress } from './onKeyPress'
 import { useHideScroll } from './useHideScroll'
 import { useWindowSize } from './useWindowSize'
+import { useClickOutside } from './useClickOutside'
 
-export { useDark, onKeyPress, useHideScroll, useWindowSize }
+export { useDark, onKeyPress, useHideScroll, useWindowSize, useClickOutside }
